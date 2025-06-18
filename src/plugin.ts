6aa63d61768774e68485dddadab62bc807e7288f
Tony <thefool363@gmail.com>
2025-06-18 17:24:33 -0400

    editing plugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,23 @@
 import { type Plugin } from '@elizaos/core';
 import { GammaService } from './services/gammaService';
+import { ClobService } from './services/clobService';
 import { readMarketsAction } from './actions/readMarkets';
 import { readMarketAction } from './actions/readMarket';
+import { buySharesAction } from './actions/buyShares';
+import { sellSharesAction } from './actions/sellShares';
+import { redeemWinningsAction } from './actions/redeemWinnings';
 
 const pluginPolymarket: Plugin = {
   name: 'plugin-polymarket',
   description: 'Plugin for Polymarket integration',
-  actions: [readMarketsAction, readMarketAction],
+  actions: [
+    readMarketsAction, 
+    readMarketAction,
+    buySharesAction,
+    sellSharesAction,
+    redeemWinningsAction
+  ],
+  services: [GammaService, ClobService],
 };
 
 export default pluginPolymarket;
